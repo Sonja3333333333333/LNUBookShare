d9ba67bf232550f26bb43ee8887100769da768f1
Sonja3333333333333 <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2025-11-04 17:58:26 +0200

    added profile window bll

---
+++ LNUBookShare/LNUBookShareBLL/Features/Books/DeleteBookCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace LNUBookShareBLL.Features.Books
+{
+    public class DeleteBookCommand : IRequest
+    {
+        public int BookId { get; set; }
+
+        /// <summary>
+        /// ID користувача, який намагається видалити.
+        /// (Для перевірки, чи він є власником)
+        /// </summary>
+        public int CurrentUserId { get; set; }
+    }
+}
---
+++ LNUBookShare/LNUBookShareBLL/Features/Profile/GetProfileQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using LNUBookShareBLL.DTOs;
+
+namespace LNUBookShareBLL.Features.Profile
+{
+    public class GetProfileQuery : IRequest<ProfileDto>
+    {
+        // Id користувача, чий профіль ми хочемо подивитися
+        public int UserId { get; set; }
+    }
+}
