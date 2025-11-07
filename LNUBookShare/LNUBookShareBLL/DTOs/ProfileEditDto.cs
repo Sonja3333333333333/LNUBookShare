b4190d706cd08dfca23e570172e9d6390623e191
Sonja3333333333333 <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2025-11-07 08:58:14 +0200

    added dll fror profile edit

---
+++ LNUBookShare/LNUBookShareBLL/DTOs/ProfileEditDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LNUBookShareBLL.DTOs
+{
+
+    public class ProfileEditDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int FacultyId { get; set; }
+        // Ми не повертаємо email, оскільки його не можна змінювати.
+    }
+}
---
+++ LNUBookShare/LNUBookShareBLL/Features/Profile/GetProfileForEditQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+//using LNUBookShareBLL.Dtos;
+using LNUBookShareBLL.DTOs;
+
+namespace LNUBookShareBLL.Features.Profile
+{
+    /// <summary>
+    /// Запит на отримання поточних даних користувача для редагування.
+    /// </summary>
+    public class GetProfileForEditQuery : IRequest<ProfileEditDto>
+    {
+        public int UserId { get; set; }
+    }
+}
